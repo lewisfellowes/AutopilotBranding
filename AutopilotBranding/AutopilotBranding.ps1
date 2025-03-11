a7e870156b4967893b511acfb4adf12501698888
LewisOSF <lewisfellowes@icloud.com>
2025-03-11 00:36:12 +0000

    Modify for personal use

--- AutopilotBranding/AutopilotBranding.ps1
+++ AutopilotBranding/AutopilotBranding.ps1
@@ -1,5 +1,5 @@
 
-function Log() {
+function LogFile() {
 	[CmdletBinding()]
 	param (
 		[Parameter(Mandatory=$false)] [String] $message
@@ -10,214 +10,80 @@ function Log() {
 }
 
 # If we are running as a 32-bit process on an x64 system, re-launch as a 64-bit process
-if ("$env:PROCESSOR_ARCHITEW6432" -ne "ARM64")
-{
-    if (Test-Path "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe")
-    {
+if ("$env:PROCESSOR_ARCHITEW6432" -ne "ARM64") {
+    if (Test-Path "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe") {
         & "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe" -ExecutionPolicy bypass -NoProfile -File "$PSCommandPath"
         Exit $lastexitcode
     }
 }
 
 # Create output folder
-if (-not (Test-Path "$($env:ProgramData)\Microsoft\AutopilotBranding"))
-{
+if (-not (Test-Path "$($env:ProgramData)\Microsoft\AutopilotBranding")) {
     Mkdir "$($env:ProgramData)\Microsoft\AutopilotBranding" -Force
 }
 
 # Start logging
 Start-Transcript "$($env:ProgramData)\Microsoft\AutopilotBranding\AutopilotBranding.log"
 
-# Creating tag file
-Set-Content -Path "$($env:ProgramData)\Microsoft\AutopilotBranding\AutopilotBranding.ps1.tag" -Value "Installed"
-
 # PREP: Load the Config.xml
 $installFolder = "$PSScriptRoot\"
-Log "Install folder: $installFolder"
-Log "Loading configuration: $($installFolder)Config.xml"
+LogFile "Install folder: $installFolder"
+LogFile "Loading configuration: $($installFolder)Config.xml"
 [Xml]$config = Get-Content "$($installFolder)Config.xml"
 
-# STEP 1: Apply a custom start menu and taskbar layout
-$ci = Get-ComputerInfo
-if ($ci.OsBuildNumber -le 22000) {
-	Log "Importing layout: $($installFolder)Layout.xml"
-	Copy-Item "$($installFolder)Layout.xml" "C:\Users\Default\AppData\Local\Microsoft\Windows\Shell\LayoutModification.xml" -Force
-} else {
-	Log "Importing Start menu layout: $($installFolder)Start2.bin"
-	MkDir -Path "C:\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState" -Force -ErrorAction SilentlyContinue | Out-Null
-	Copy-Item "$($installFolder)Start2.bin" "C:\Users\Default\AppData\Local\Packages\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\LocalState\Start2.bin" -Force
-	Log "Importing Taskbar layout: $($installFolder)TaskbarLayoutModification.xml"
-	MkDir -Path "C:\Windows\OEM\" -Force -ErrorAction SilentlyContinue | Out-Null
-	Copy-Item "$($installFolder)TaskbarLayoutModification.xml" "C:\Windows\OEM\TaskbarLayoutModification.xml" -Force
-	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v LayoutXMLPath /t REG_EXPAND_SZ /d "%SystemRoot%\OEM\TaskbarLayoutModification.xml" /f | Out-Host
-	Log "Unpin the Microsoft Store app from the taskbar"
-	reg.exe load HKLM\TempUser "C:\Users\Default\NTUSER.DAT" | Out-Host
-	reg.exe add "HKLM\TempUser\Software\Policies\Microsoft\Windows\Explorer" /v NoPinningStoreToTaskbar /t REG_DWORD /d 1 /f | Out-Host
-	reg.exe unload HKLM\TempUser | Out-Host
-}
 
-# STEP 2: Configure background
-reg.exe load HKLM\TempUser "C:\Users\Default\NTUSER.DAT" | Out-Host
-
-Log "Setting up Autopilot theme"
-Mkdir "C:\Windows\Resources\OEM Themes" -Force | Out-Null
-Copy-Item "$installFolder\Autopilot.theme" "C:\Windows\Resources\OEM Themes\Autopilot.theme" -Force
-Mkdir "C:\Windows\web\wallpaper\Autopilot" -Force | Out-Null
-Copy-Item "$installFolder\Autopilot.jpg" "C:\Windows\web\wallpaper\Autopilot\Autopilot.jpg" -Force
-Log "Setting Autopilot theme as the new user default"
-reg.exe add "HKLM\TempUser\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes" /v InstallTheme /t REG_EXPAND_SZ /d "%SystemRoot%\resources\OEM Themes\Autopilot.theme" /f | Out-Host
-
-# STEP 2A: Stop Start menu from opening on first logon
-reg.exe add "HKLM\TempUser\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v StartShownOnUpgrade /t REG_DWORD /d 1 /f | Out-Host
-
-# STEP 2B: Hide "Learn more about this picture" from the desktop
-reg.exe add "HKLM\TempUser\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel" /v "{2cc5ca98-6485-489a-920e-b3e88a6ccce3}" /t REG_DWORD /d 1 /f | Out-Host
-
-# STEP 3C: Disable Windows Spotlight as per https://github.com/mtniehaus/AutopilotBranding/issues/13#issuecomment-2449224828
-Log "Disabling Windows Spotlight for Desktop"
-reg.exe add "HKLM\TempUser\Software\Policies\Microsoft\Windows\CloudContent" /v DisableSpotlightCollectionOnDesktop /t REG_DWORD /d 1 /f | Out-Host
-
-reg.exe unload HKLM\TempUser | Out-Host
-
-# STEP 3: Set time zone (if specified)
+# Set time zone
 if ($config.Config.TimeZone) {
-	Log "Setting time zone: $($config.Config.TimeZone)"
-	Set-Timezone -Id $config.Config.TimeZone
-}
-else {
+	LogFile "Setting time zone: $($config.Config.TimeZone)"
+	try {
+		Set-Timezone -Id $config.Config.TimeZone
+        LogFile "SUCCESS: Time zone has been added. $_"
+	} catch {
+        LogFile "ERROR: Failed to set time zone. $_"
+	}
+} else {
 	# Enable location services so the time zone will be set automatically (even when skipping the privacy page in OOBE) when an administrator signs in
+	LogFile "No time zone configured. Enabling location services..."
 	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location" -Name "Value" -Type "String" -Value "Allow" -Force
 	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}" -Name "SensorPermissionState" -Type "DWord" -Value 1 -Force
 	Start-Service -Name "lfsvc" -ErrorAction SilentlyContinue
+	LogFile "Location Services enabled."
 }
 
-# STEP 4: Remove specified provisioned apps if they exist
-Log "Removing specified in-box provisioned apps"
-$apps = Get-AppxProvisionedPackage -online
-$config.Config.RemoveApps.App | % {
-	$current = $_
-	$apps | ? {$_.DisplayName -eq $current} | % {
-		try {
-			Log "Removing provisioned app: $current"
-			$_ | Remove-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue | Out-Null
-		} catch { }
+# Add language packs
+Get-ChildItem "$($installFolder)LPs" -Filter *.cab | % {
+	LogFile "Adding language pack: $($_.FullName)"
+	try {
+		Add-WindowsPackage -Online -NoRestart -PackagePath $_.FullName -ErrorAction Stop
+        LogFile "Success: Language pack has been added. $_"
+	} catch {
+		LogFile "ERROR: Failed to add language pack. $_"
 	}
 }
 
-# STEP 5: Install OneDrive per machine
-if ($config.Config.OneDriveSetup) {
-	Log "Downloading OneDriveSetup"
-	$dest = "$($env:TEMP)\OneDriveSetup.exe"
-	$client = new-object System.Net.WebClient
-	$client.DownloadFile($config.Config.OneDriveSetup, $dest)
-	Log "Installing: $dest"
-	$proc = Start-Process $dest -ArgumentList "/allusers" -WindowStyle Hidden -PassThru
-	$proc.WaitForExit()
-	Log "OneDriveSetup exit code: $($proc.ExitCode)"
-}
-
-# STEP 6: Don't let Edge create a desktop shortcut (roams to OneDrive, creates mess)
-Log "Turning off (old) Edge desktop shortcut"
-reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v DisableEdgeDesktopShortcutCreation /t REG_DWORD /d 1 /f /reg:64 | Out-Host
-
-# STEP 7: Add language packs
-Get-ChildItem "$($installFolder)LPs" -Filter *.cab | % {
-	Log "Adding language pack: $($_.FullName)"
-	Add-WindowsPackage -Online -NoRestart -PackagePath $_.FullName
-}
-
-# STEP 8: Change language
+# Change language
 if ($config.Config.Language) {
-	Log "Configuring language using: $($config.Config.Language)"
+	LogFile "Configuring language using: $($config.Config.Language)"
 	& $env:SystemRoot\System32\control.exe "intl.cpl,,/f:`"$($installFolder)$($config.Config.Language)`""
 }
 
-# STEP 9: Add features on demand, Disable Optional Features, Remove Windows Capabilities
-$currentWU = (Get-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -ErrorAction Ignore).UseWuServer
-if ($currentWU -eq 1)
-{
-	Log "Turning off WSUS"
-	Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU"  -Name "UseWuServer" -Value 0
-	Restart-Service wuauserv
-}
-# Step 9A: Add features on demand
-if ($config.Config.AddFeatures.Feature.Count -gt 0)
-{
-	$config.Config.AddFeatures.Feature | % {
-		Log "Adding Windows feature: $_"
-		Add-WindowsCapability -Online -Name $_ -ErrorAction SilentlyContinue | Out-Null
-	}
-}
-# Step 9B: Disable Optional features
-if ($config.Config.DisableOptionalFeatures.Feature.Count -gt 0)
-{
-	$EnabledOptionalFeatures = Get-WindowsOptionalFeature -Online | Where-Object {$_.State -eq "Enabled"}
-	foreach ($EnabledFeature in $EnabledOptionalFeatures) {
-		if ($config.Config.DisableOptionalFeatures.Feature -contains $EnabledFeature.FeatureName) {
-			Log "Disabling Optional Feature:  $($EnabledFeature.FeatureName)"
-			Disable-WindowsOptionalFeature -Online -FeatureName $EnabledFeature.FeatureName -NoRestart | Out-Null
-		}
-	}
-}
-# Step 9C: Remove Windows Capabilities
-if ($config.Config.RemoveCapability.Capability.Count -gt 0)
-{
-	$InstalledCapabilities = Get-WindowsCapability -Online | Where-Object {$_.State -eq "Installed"}
-	foreach ($InstalledCapability in $InstalledCapabilities) {
-		if ($config.Config.RemoveCapability.Capability -contains $InstalledCapability.Name.Split("~")[0]) {
-			Log "Removing Windows Capability:  $($InstalledCapability.Name)"
-			Remove-WindowsCapability -Online -Name $InstalledCapability.Name  | Out-Null
-		}
-	}
+# Don't let Edge create a desktop shortcut (roams to OneDrive, creates mess)
+try {
+	LogFile "Turning off (old) Edge desktop shortcut"
+	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /v DisableEdgeDesktopShortcutCreation /t REG_DWORD /d 1 /f /reg:64 | Out-Host
+} catch {
+	LogFile "ERROR: Failed to modify Edge shortcut registry. $_"
 }
 
-if ($currentWU -eq 1)
-{
-	Log "Turning on WSUS"
-	Set-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU"  -Name "UseWuServer" -Value 1
-	Restart-Service wuauserv
+# Disable new Edge desktop icon
+try {
+LogFile "Turning off Edge desktop icon"
+		reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\EdgeUpdate" /v "CreateDesktopShortcutDefault" /t REG_DWORD /d 0 /f /reg:64 | Out-Host
+} catch {
+	LogFile "ERROR: Failed to modify Edge icon registry. $_"
 }
 
-# STEP 10: Customize default apps
-if ($config.Config.DefaultApps) {
-	Log "Setting default apps: $($config.Config.DefaultApps)"
-	& Dism.exe /Online /Import-DefaultAppAssociations:`"$($installFolder)$($config.Config.DefaultApps)`"
-}
-
-# STEP 11: Set registered user and organization
-Log "Configuring registered user information"
-reg.exe add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v RegisteredOwner /t REG_SZ /d "$($config.Config.RegisteredOwner)" /f /reg:64 | Out-Host
-reg.exe add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v RegisteredOrganization /t REG_SZ /d "$($config.Config.RegisteredOrganization)" /f /reg:64 | Out-Host
-
-# STEP 12: Configure OEM branding info
-if ($config.Config.OEMInfo)
-{
-	Log "Configuring OEM branding info"
-
-	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v Manufacturer /t REG_SZ /d "$($config.Config.OEMInfo.Manufacturer)" /f /reg:64 | Out-Host
-	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v Model /t REG_SZ /d "$($config.Config.OEMInfo.Model)" /f /reg:64 | Out-Host
-	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v SupportPhone /t REG_SZ /d "$($config.Config.OEMInfo.SupportPhone)" /f /reg:64 | Out-Host
-	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v SupportHours /t REG_SZ /d "$($config.Config.OEMInfo.SupportHours)" /f /reg:64 | Out-Host
-	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v SupportURL /t REG_SZ /d "$($config.Config.OEMInfo.SupportURL)" /f /reg:64 | Out-Host
-	Copy-Item "$installFolder\$($config.Config.OEMInfo.Logo)" "C:\Windows\$($config.Config.OEMInfo.Logo)" -Force
-	reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation" /v Logo /t REG_SZ /d "C:\Windows\$($config.Config.OEMInfo.Logo)" /f /reg:64 | Out-Host
-}
-
-# STEP 13: Enable UE-V
-Log "Enabling UE-V"
-Enable-UEV
-Set-UevConfiguration -Computer -SettingsStoragePath "%OneDriveCommercial%\UEV" -SyncMethod External -DisableWaitForSyncOnLogon
-Get-ChildItem "$($installFolder)UEV" -Filter *.xml | % {
-	Log "Registering template: $($_.FullName)"
-	Register-UevTemplate -Path $_.FullName
-}
-
-# STEP 14: Disable network location fly-out
-Log "Turning off network location fly-out"
-reg.exe add "HKLM\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff" /f
-
-# STEP 15: Disable new Edge desktop icon
-Log "Turning off Edge desktop icon"
-reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\EdgeUpdate" /v "CreateDesktopShortcutDefault" /t REG_DWORD /d 0 /f /reg:64 | Out-Host
+# Creating tag file
+Set-Content -Path "$($env:ProgramData)\Microsoft\AutopilotBranding\AutopilotBranding.ps1.tag" -Value "Installed"
 
 Stop-Transcript
